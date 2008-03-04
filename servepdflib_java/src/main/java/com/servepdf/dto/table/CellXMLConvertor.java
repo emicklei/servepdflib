819ddf8e088de63111da22c607f23182b48f8e33
edwineve <edwineve@a4f59473-7844-0410-b1be-1394d8931193>
2008-03-04 20:21:31 +0000

    table stuff

--- servepdflib_java/src/main/java/com/servepdf/dto/table/CellXMLConvertor.java
+++ servepdflib_java/src/main/java/com/servepdf/dto/table/CellXMLConvertor.java
@@ -1,14 +1,30 @@
 package com.servepdf.dto.table;
 
-import com.thoughtworks.xstream.converters.basic.AbstractSingleValueConverter;
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 
-public class CellXMLConvertor extends AbstractSingleValueConverter {
+public class CellXMLConvertor implements Converter {
 
 	public boolean canConvert(Class clazz) {
         return Cell.class.isAssignableFrom(clazz);
 	}
 
-	public Object fromString(String str) {
-        return new Cell(str);
+    public void marshal(Object value, HierarchicalStreamWriter writer,
+            MarshallingContext context) {
+    	Cell c = (Cell) value;
+    	writer.addAttribute("font", c.font);
+    	writer.addAttribute("fontSize", c.fontSize);
+    	writer.setValue(c.contents);
+    }
+
+public Object unmarshal(HierarchicalStreamReader reader,
+            UnmarshallingContext context) {           
+    	reader.moveDown();
+    	Cell c = new Cell(reader.getValue(),reader.getAttribute("font"),reader.getAttribute("fontSize"));
+    	reader.moveUp();
+    	return c;
 	}
 }
--- servepdflib_java/src/test/java/com/servepdf/dto/table/BuilderTest.java
+++ servepdflib_java/src/test/java/com/servepdf/dto/table/BuilderTest.java
@@ -6,11 +6,11 @@ public class BuilderTest extends TestCase {
 	
 	public void testOneCell(){
 		Builder b = new Builder();
-		b.tr();
+		b.tr("10");
 		b.th("Title");
-		b.tr();
+		b.tr("10");
 		b.td("ServePDF");
-		b.tr();
+		b.tr("10");
 		System.out.println(b.table);
 	}
 }
