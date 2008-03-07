c1dd2d257e39aef6a18abc9b3b02de73ae6de576
edwineve <edwineve@a4f59473-7844-0410-b1be-1394d8931193>
2008-03-07 11:50:30 +0000

    fromxml

--- trunk/servepdflib_java/src/main/java/com/servepdf/dto/table/Table.java
+++ trunk/servepdflib_java/src/main/java/com/servepdf/dto/table/Table.java
@@ -10,6 +10,14 @@ public class Table {
 	public String columnWidths;
     public String field;
 	
+	/**
+	 * Create a new Table from the XML representation of Table.
+	 * @return Table
+	 */
+	public static Table fromXML(String xml) {
+		return (Table) (Table.newXStream().fromXML(xml));
+	}
+	
     public String toString(){
         return this.toXML();
     }
@@ -23,16 +31,16 @@ public class Table {
         xstream.alias("table", Table.class);
         xstream.useAttributeFor("columnWidths", String.class);
         xstream.useAttributeFor("field", String.class);
+        xstream.registerConverter(new CellXMLConvertor());
         xstream.alias("tr", Row.class);
         xstream.alias("th", HeaderCell.class);
         xstream.alias("td", Cell.class);
         xstream.addImplicitCollection(Table.class, "rows");
         xstream.addImplicitCollection(Row.class, "cells");
-        xstream.useAttributeFor("height", String.class);
-        xstream.useAttributeFor("font", String.class);
-        xstream.useAttributeFor("fontSize", String.class);
-        //xstream.aliasField("", Cell.class, "contents");
-        xstream.registerConverter(new CellXMLConvertor());            
+//        xstream.useAttributeFor("height", String.class);
+//        xstream.useAttributeFor("font", String.class);
+//        xstream.useAttributeFor("fontSize", String.class);
+            
        
         return xstream;
 	}
