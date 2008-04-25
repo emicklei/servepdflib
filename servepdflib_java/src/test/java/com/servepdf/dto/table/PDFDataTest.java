65ddb0e7fb420cbf1fe294d0daccb7b64679bd78
ernest.micklei <ernest.micklei@a4f59473-7844-0410-b1be-1394d8931193>
2008-04-25 18:14:11 +0000

    add more test assertions

--- servepdflib_java/src/test/java/com/servepdf/dto/table/PDFDataTest.java
+++ servepdflib_java/src/test/java/com/servepdf/dto/table/PDFDataTest.java
@@ -1,5 +1,7 @@
 package com.servepdf.dto.table;
 
+import com.servepdf.dto.Content;
+import com.servepdf.dto.Head;
 import com.servepdf.dto.PDFData;
 import com.servepdf.dto.TextField;
 
@@ -25,16 +27,25 @@ public class PDFDataTest extends TestCase {
 	    data.head.accessKey = "accessKey";
 	    data.head.applicationKey = "applicationKey";
 	    data.head.documentName = "outputFileName";
+	    data.head.cacheControl = Head.CACHE_ASSETS;
 	    data.head.unserviceableURL = "unserviceableURL";
 		data.addText("test", "value");
-		data.body.contentList.get(0).formURL = "http://here.there.com";
+		Content content = data.body.contentList.get(0);
+		content.formURL = "http://here.there.com";
 		Table table = new Table();
 		table.columnWidths = "100,10";
 		table.setFontSize("12");
         data.body.contentList.get(0).getTables().add(table);
-		data = toXmlAndBack(data);
-
-		TextField tf = (TextField)(data.body.getContent().getFields().get(0));
+		PDFData atad = toXmlAndBack(data);
+		// test if everything was read
+		assertEquals(data.head.accessKey, atad.head.accessKey);
+		assertEquals(data.head.applicationKey, atad.head.applicationKey);
+		assertEquals(data.head.documentName, atad.head.documentName);
+		assertEquals(data.head.unserviceableURL, atad.head.unserviceableURL);
+		assertEquals(data.head.cacheControl, atad.head.cacheControl);
+		Content tentnoc = atad.body.getContent();
+		assertEquals(content.formURL, tentnoc.formURL);
+		TextField tf = (TextField)(content.getFields().get(0));
 		assertNotNull(tf);
 		assertEquals("test",tf.name);
 		assertEquals("value",tf.value);
