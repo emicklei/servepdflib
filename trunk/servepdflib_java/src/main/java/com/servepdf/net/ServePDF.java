cc9a96df7b4140efae9a14eb52e9a2ed7308ff7f
ernest.micklei <ernest.micklei@a4f59473-7844-0410-b1be-1394d8931193>
2008-10-30 18:28:47 +0000

    1.0.1 = add feed method for ServePDF

--- trunk/servepdflib_java/src/main/java/com/servepdf/net/ServePDF.java
+++ trunk/servepdflib_java/src/main/java/com/servepdf/net/ServePDF.java
@@ -15,10 +15,12 @@
 */
 package com.servepdf.net;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.StringRequestEntity;
@@ -26,37 +28,65 @@ import org.apache.commons.httpclient.methods.StringRequestEntity;
 import com.servepdf.dto.PDFData;
 
 public class ServePDF {
+	public static int RESPONSE_BUFFER_LENGTH = 10240; // 10Kb
+	public static String UNSERVICEABLE_URL = "";
 	
 	public String base_url = "http://pws.servepdf.com";
 	private HttpClient http  = new HttpClient();
+	
+	public void feed(String feedUrl, String formUrl, int maxEntries, OutputStream out, String secretAccessKey) {
+		if (feedUrl == null || feedUrl.length() == 0)
+			throw new RuntimeException("Missing feedUrl parameter value");
+		if (formUrl == null || formUrl.length() == 0)
+			throw new RuntimeException("Missing formUrl parameter value");
+		if (secretAccessKey == null || secretAccessKey.length() == 0)
+			throw new RuntimeException("Missing secretAccessKey parameter value");		
+		PostMethod post = new PostMethod(base_url + "/rss");
+		post.addParameter("formURL", formUrl);
+		post.addParameter("feedURL", feedUrl);
+		post.addParameter("maxEntries", String.valueOf(Math.min(maxEntries,1)));
+		post.addParameter("unserviceableURL",UNSERVICEABLE_URL);
+		try {
+			String authentication = this.computeSignature(null,secretAccessKey);
+			receiveAfterPost(out, post, authentication);
+		} catch (Exception ex) {
+			throw new RuntimeException("Unable to process RSS feed",ex);
+		} finally {
+			post.releaseConnection();
+		}
+	}
 	
 	public void process(PDFData data, OutputStream out, String secretAccessKey){
 		PostMethod post = new PostMethod(base_url + "/process");		
 		try {
 			String authentication = this.computeSignature(data,secretAccessKey);
 			post.setRequestEntity(new StringRequestEntity(data.toXML(),"text/xml" , "utf8"));			
-			// flex clients no longer can send the Authorization Http Header
-			// so a custom header need to be used.
-			post.setRequestHeader("pws-authentication", authentication);
-			int code = this.http.executeMethod(post);
-			if (code != HttpStatus.SC_OK) 
-				throw new RuntimeException("post failed returning http code:" + code);
-			InputStream pdfStream =  post.getResponseBodyAsStream();
-			byte[] buffer = new byte[1024];
-			int howMany = 0;
-			do {
-				howMany = pdfStream.read(buffer);
-				if (howMany > 0) out.write(buffer, 0, howMany);
-			} while (howMany != -1);
+			receiveAfterPost(out, post, authentication);
 		} catch (Exception ex) {
 			throw new RuntimeException("Unable to process PDFData",ex);
 		} finally {
 			post.releaseConnection();
 		}		
 	}
+
+	private void receiveAfterPost(OutputStream out, PostMethod post, String authentication) throws IOException, HttpException {
+		// flex clients no longer can send the Authorization Http Header
+		// so a custom header need to be used.
+		post.setRequestHeader("pws-authentication", authentication);
+		int code = this.http.executeMethod(post);
+		if (code != HttpStatus.SC_OK) 
+			throw new RuntimeException("post failed returning http code:" + code);
+		InputStream pdfStream =  post.getResponseBodyAsStream();
+		byte[] buffer = new byte[RESPONSE_BUFFER_LENGTH];
+		int howMany = 0;
+		do {
+			howMany = pdfStream.read(buffer);
+			if (howMany > 0) out.write(buffer, 0, howMany);
+		} while (howMany != -1);
+	}
 	
 	private String computeSignature(PDFData data,String secretAccessKey) {
-		// TOOD
+		// TODO
 		return secretAccessKey;
 	}
 }
